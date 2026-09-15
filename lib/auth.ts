0905931dcb4c844048a689ccbe6cf5c26b388f17
Islam CHEBAHI <chebahiislam43@gmail.com>
2026-09-15 12:51:17 +0200

    fix nextauth production cookie

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,20 @@ import { getServerSession } from "next-auth";
 import { prisma } from "@/lib/prisma";
 
 export const authOptions: NextAuthOptions = {
+  cookies: {
+    sessionToken: {
+      name:
+        process.env.NODE_ENV === "production"
+          ? "__Secure-next-auth.session-token"
+          : "next-auth.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
   session: {
     strategy: "jwt",
   },
